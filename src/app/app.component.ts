74cce804c493a1ccdb83e092a5ac853470c3e137
= <=>
2017-10-15 13:46:55 -0200

    em andamento montagem heroi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,19 @@ export class Hero {
   Nome: string;
 }
 
+const Herois : Hero [] = [
+  {Id: 1, Nome: "Graves"},
+  {Id: 2, Nome: "Ashe"},
+  {Id: 3, Nome:}
+]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo: 'Tour of Heroes';
+  titulo: 'Tour of Heroes - League of Legends';
   heroi: Hero = {
     Id: 1,
     Nome: 'Jean Grey'
