454be1508c7a0bb1291fb07ad36fdedfba89e973
= <=>
2017-10-15 13:20:58 -0200

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  Id: number;
+  Nome: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  titulo: 'Tour of Heroes';
+  heroi: Hero = {
+    Id: 1,
+    Nome: 'Jean Grey'
+  }
+
 }
