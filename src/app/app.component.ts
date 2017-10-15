eb48a463f70841b49d8fd9984580d4e1eda6a776
= <=>
2017-10-15 16:21:03 -0200

    Commit Lista Campeoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 
-export class Hero {
+export class Campeao {
   Id: number;
   Nome: string;
+  Titulo: string;
+  Tipo: string;
+  Regiao: string;
 }
 
-const Herois : Hero [] = [
-  {Id: 1, Nome: "Graves"},
-  {Id: 2, Nome: "Ashe"},
-  {Id: 3, Nome:}
-]
+const CAMPEOES: Campeao [] = [
+  {Id: 1, Nome: "Graves", Titulo: "O Foragido", Tipo: "Atirador", Regiao: "Bilgewater"},
+  {Id: 2, Nome: "Ashe", Titulo: "A Arqueira do Gelo", Tipo: "Atirador", Regiao: "Freljord"},
+  {Id: 3, Nome: "Annie", Titulo: "A Criança Sombria", Tipo: "Mago", Regiao: "Independente"},
+  {Id: 4, Nome: "Darius", Titulo: "A Mão de Noxus", Tipo: "Tanque", Regiao: "Noxus"},
+  {Id: 5, Nome: "Garen", Titulo: "O Poder de Demacia", Tipo: "Tanque", Regiao: "Demacia"},
+  {Id: 6, Nome: "Jarvan IV", Titulo: "O Exemplo de Demacia", Tipo: "Tanque", Regiao: "Demacia"},
+  {Id: 7, Nome: "Poppy", Titulo: "Guardiã do Martelo", Tipo: "Tanque", Regiao: "Bandle City / Demacia"},
+  {Id: 8, Nome: "Twisted Fate", Titulo: "O Mestre das Cartas", Tipo: "Mago", Regiao: "Bilgewater"},
+  {Id: 9, Nome: "Volibear", Titulo: "O Rugido do Trovão", Tipo: "Tanque", Regiao: "Freljord"},
+  {Id: 10, Nome: "Braum", Titulo: "O Coração de Freljord", Tipo: "Tanque", Regiao: "Freljord"}
+];
 
 @Component({
   selector: 'app-root',
@@ -18,9 +28,6 @@ const Herois : Hero [] = [
 })
 export class AppComponent {
   titulo: 'Tour of Heroes - League of Legends';
-  heroi: Hero = {
-    Id: 1,
-    Nome: 'Jean Grey'
-  }
+  campeoes = CAMPEOES;
 
 }
